4ca273d50f045f0d819714509628407c9b0e29cb
Vitaly Gashkov <vitalygashkov@vk.com>
2024-12-01 18:12:13 +0500

    refactor: migrate to new api

--- service.test.js
+++ service.test.js
@@ -1,24 +1,26 @@
 // CommonJS syntax is not supported / deprecated by Vitest, so using ESM
 
+import { join } from 'node:path';
 import { expect, test } from 'vitest';
-import { registerService } from '@streamyx/core';
-import service from '../lib/service';
+import { install } from '@streamyx/core';
 
-test('register service', () => {
-  const instance = registerService(service);
-  expect(instance).toBeDefined();
-  expect(instance).toHaveProperty('name', 'ntv');
-  expect(instance).toHaveProperty('fetchContentMetadata');
+const filePath = join(__dirname, './service.js');
+
+test('install', async () => {
+  const service = await install(filePath);
+  expect(service).toBeDefined();
+  expect(service).toHaveProperty('name', 'ntv');
+  expect(service).toHaveProperty('fetchContentMetadata');
 });
 
 test('fetch content metadata', async () => {
-  const instance = registerService(service);
-  const response = await instance.fetchContentMetadata('https://www.ntv.ru/video/2366302');
+  const service = await install(filePath);
+  const response = await service.fetchContentMetadata('https://www.ntv.ru/video/2366302');
   expect(response).toBeDefined();
   expect(response).toHaveLength(1);
   expect(response[0]).toHaveProperty('source');
   expect(response[0].source).toHaveProperty('url');
   const expectedSourceUrl =
-    'https://cdn2-vod-mp4.ntv.ru/news/2024/20240916/19_Chernov_hq87DTcVGUaKkKIVbE.mp4?e=1726592122&md5=3j9IdLADMJWzUktx8aKPKw';
+    'https://ymedia-k.ntv.ru/news/2024/20240916/19_Chernov_hq87DTcVGUaKkKIVbE.mp4?ts=1733145091&md5=LG44V6VKZMFf-6cvvKukDQ';
   expect(response[0].source?.url).toBe(expectedSourceUrl);
 });
